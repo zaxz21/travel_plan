3b2f0f241ef041ef8fe7d0d995c9a407978c4991
christmasboy <zaxz21@naver.com>
2023-02-09 01:26:23 +0900

    first

---
+++ src/main/java/com/greenart/travle_plan/TravlePlanApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.travle_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravlePlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenart/travle_plan/TravlePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart.travle_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravlePlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
