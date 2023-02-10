c55adef8e2516553f87ed64d2c7934ce6921a0b4
eun <eu-nee@naver.com>
2023-02-10 18:07:03 +0900

    [정은]

--- src/main/java/com/greenart/travle_plan/repository/ZoneConnectionRepository.java
+++ src/main/java/com/greenart/travle_plan/repository/ZoneConnectionRepository.java
@@ -1,9 +1,10 @@
 package com.greenart.travle_plan.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greenart.travle_plan.entity.ZoneConnectionEntity;
 
-
 public interface ZoneConnectionRepository extends JpaRepository<ZoneConnectionEntity, Long>{
+    ZoneConnectionEntity findBySeq(Long seq);
     
 }
--- src/test/java/com/greenart/travle_plan/TravlePlanApplicationTests.java
+++ src/test/java/com/greenart/travle_plan/TravlePlanApplicationTests.java
@@ -1,13 +1,19 @@
 package com.greenart.travle_plan;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.greenart.travle_plan.repository.ZoneConnectionRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class TravlePlanApplicationTests {
-
+@Autowired ZoneConnectionRepository zoneConnectionRepository;
 	@Test
-	void contextLoads() {
+	void getZone(){
+		System.out.println(zoneConnectionRepository.findAll());
 	}
-
 }
