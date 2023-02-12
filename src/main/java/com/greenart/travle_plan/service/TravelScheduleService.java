3b596dcca5b9edbbc70b77b8a81ca79515f8ef76
christmasboy <zaxz21@naver.com>
2023-02-13 01:32:21 +0900

    20230212

---
+++ src/main/java/com/greenart/travle_plan/service/TravelScheduleService.java
@@ -0,0 +1,11 @@
+package com.greenart.travle_plan.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TravelScheduleService {
+    
+}
